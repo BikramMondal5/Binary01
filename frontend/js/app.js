00ba3999db47fe6f0ceb5550bd6f9c38809fe929
BikramMondal5 <codesnippets45@gmail.com>
2025-03-09 01:41:08 +0530

    msg

---
+++ frontend/js/app.js
@@ -0,0 +1,7 @@
+document.getElementById("Button1").addEventListener("click", function() {
+    window.location.href = "creators.html"; 
+});
+
+document.getElementById("Button2").addEventListener("click", function() {
+    window.location.href = "learners.html"; 
+});
